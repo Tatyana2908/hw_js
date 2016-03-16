74a55918221f2ba85343b39c109d2b812006255d
Tatyana Prokhorenko <tatyana.2908@hotmail.com>
2016-03-16 17:37:47 +0200

    added hw_3-4

---
+++ js_module_3-4/js/module_3-4.js
@@ -0,0 +1,59 @@
+
+
+var data = {
+    'title': 'Programming test',
+    'questionsList': ['Question 1', 'Question 2', 'Question 3'],
+    'answersList': ['Answer option 1', 'Answer option 2', 'Answer option 3'],
+};
+
+//buildHeader = function(){
+var wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+    document.body.appendChild(wrapper);
+
+var titleH1 = document.createElement('h3');
+    titleH1.innerHTML = data.title;
+    wrapper.appendChild(titleH1);
+
+var formBox = document.createElement('form');
+    wrapper.appendChild(formBox);
+
+for (var i = 0; i < data.questionsList.length; i++) {
+    var questionnaire = document.createElement('ul');
+    questionnaire.innerHTML = data.questionsList[i];
+    formBox.appendChild(questionnaire);
+
+
+    for (var j = 0; j < data.answersList.length; j++) {
+        var list = document.createElement('li');
+        questionnaire.appendChild(list);
+
+        var labelFor = document.createElement('label');
+        labelFor.htmlFor = "checkboxid_" + i+  "_" +j;
+        labelFor.innerHTML = data.answersList[j];
+
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.id = "checkboxid_" + i+  "_" +j;
+        list.appendChild(checkbox );
+        list.appendChild(labelFor );
+    }
+}
+
+var buttonSubmit = document.createElement('button');
+    buttonSubmit.className = 'btn btn-primary';
+var textButton = document.createTextNode('check the results');
+    buttonSubmit.appendChild(textButton);
+    formBox.appendChild(buttonSubmit);
+
+
+
+
+
+
+
+
+
+//},
+
+console.log(data);
