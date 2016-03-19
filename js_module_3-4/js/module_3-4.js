1167fa166c5d7e380db6255689b0778d39cd54e8
Tatyana Prokhorenko <tatyana.2908@hotmail.com>
2016-03-19 21:40:49 +0200

    fixed

--- js_module_3-4/js/module_3-4.js
+++ js_module_3-4/js/module_3-4.js
@@ -7,49 +7,54 @@ var data = {
 };
 
 
+var page = {
+    buildHeader: function() {
+        var wrapper = document.createElement('div');
+        wrapper.className = 'wrapper';
+        document.body.appendChild(wrapper);
 
-var wrapper = document.createElement('div');
-    wrapper.className = 'wrapper';
-    document.body.appendChild(wrapper);
+        var titleH1 = document.createElement('h3');
+        titleH1.innerHTML = data.title;
+        wrapper.appendChild(titleH1);
+    },
+    buildForm: function() {
+        var formBox = document.createElement('form');
+        document.querySelector('.wrapper').appendChild(formBox);
 
-    var titleH1 = document.createElement('h3');
-    titleH1.innerHTML = data.title;
-    wrapper.appendChild(titleH1);
-
-
-
-var formBox = document.createElement('form');
-        wrapper.appendChild(formBox);
-
-    for (var i = 0; i < data.questionsList.length; i++) {
-        var questionnaire = document.createElement('ul');
-        questionnaire.innerHTML = data.questionsList[i];
-        formBox.appendChild(questionnaire);
+        for (var i = 0; i < data.questionsList.length; i++) {
+            var questionnaire = document.createElement('ul');
+            questionnaire.innerHTML = data.questionsList[i];
+            formBox.appendChild(questionnaire);
 
 
-        for (var j = 0; j < data.answersList.length; j++) {
-            var list = document.createElement('li');
-            questionnaire.appendChild(list);
+            for (var j = 0; j < data.answersList.length; j++) {
+                var list = document.createElement('li');
+                questionnaire.appendChild(list);
 
-            var labelFor = document.createElement('label');
-            labelFor.htmlFor = "checkboxid_" + i+  "_" +j;
-            labelFor.innerHTML = data.answersList[j];
+                var labelFor = document.createElement('label');
+                labelFor.htmlFor = "checkboxid_" + i + "_" + j;
+                labelFor.innerHTML = data.answersList[j];
 
-            var checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            checkbox.id = "checkboxid_" + i+  "_" +j;
-            list.appendChild(checkbox );
-            list.appendChild(labelFor );
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.id = "checkboxid_" + i + "_" + j;
+                list.appendChild(checkbox);
+                list.appendChild(labelFor);
+            }
         }
-    }
 
-    var buttonSubmit = document.createElement('input');
+        var buttonSubmit = document.createElement('input');
         buttonSubmit.type = 'submit';
         buttonSubmit.value = 'check the results';
         buttonSubmit.className = 'btn btn-primary';
         formBox.appendChild(buttonSubmit);
+    },
+    pageInit: function(){
+        this.buildHeader();
+        this.buildForm();
 
-
-
+    }
+};
+page.pageInit();
 
 console.log(data);
