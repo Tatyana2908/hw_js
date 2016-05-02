2417d1f8ff9ad988df0774f51f210fb1de139656
Tatyana Prokhorenko <tatyana.2908@hotmail.com>
2016-05-02 22:19:55 +0300

    fixed milsec

--- js_module_5-6/js/timer.js
+++ js_module_5-6/js/timer.js
@@ -19,37 +19,38 @@ var hour = 1,
 
 function startTIME() {
 	var base = 60;
-	var cdateObj = new Date(); 
-	var t = (cdateObj.getTime() - dateObj.getTime())-(sec*1000);
+	var cdateObj = new Date();
+	var time = (cdateObj.getTime() - dateObj.getTime())-(sec * 1000);
 	
-	if (t>999) { sec++; }
-	if (sec >= (min*base)) {
+	if (time > 999) { sec++; }
+	if (sec >= (min * base)) {
 		ts = 0;
 		min++;
 	} else { 
-		ts = parseInt((msec/100) + sec);
-		if(ts >= base) { ts = ts - ((min-1)*base); }
+		ts = parseInt((msec / 1000) + sec);
+		if(ts >= base) { ts = ts - ((min - 1) * base); }
 	} 
-	if (min > (hour*base)) {
+	if (min > (hour * base)) {
 		tm = 1;
 		hour++;
 	} else { 
-		tm = parseInt((msec/100) + min);
-		if(tm >= base) { tm = tm -((hour-1)*base); }
+		tm = parseInt((msec / 1000) + min);
+		if(tm >= base) { tm = tm -((hour - 1) *  base); }
 	}
-	msec = Math.round(t/10);
-	if (msec > 99) {msec = 0;}
-	if (msec == 0) {msec = '00';}
-	if (msec > 0&&msec <= 9) { msec = '0' + msec; }
-	if (ts>0) { datasec = ts; if (ts<10) { datasec = '0'+ts; }} else { datasec = '00'; }
+	msec = Math.round(time);
+	if (msec >= 999) {msec = 0;}
+	if (msec == 0) {msec = '000';}
+	if (msec > 0 && msec <= 99) { msec = '0' + msec; }
+	if (ts > 0) { datasec = ts;
+	if (ts <= 9) { datasec = '0' + ts; }} else { datasec = '00'; }
 	datamin = tm - 1;
-	if (datamin > 0) { if (datamin < 10) { datamin = '0' + datamin; }} else { datamin = '00'; }
-	dataHour = hour-1;
-	if (dataHour > 0) { if (dataHour < 10) { dataHour = '0' + dataHour; }} else { dataHour = '00'; }
+	if (datamin > 0) { if (datamin <= 9) { datamin = '0' + datamin; }} else { datamin = '00'; }
+	//dataHour = hour-1;
+	if (dataHour > 0) { if (dataHour <= 9) { dataHour = '0' + dataHour; }} else { dataHour = '00'; }
 	readout = dataHour + ':' + datamin + ':' + datasec + '.' + msec;
-	if (show==true) { document.TestForm.stopwatch.value = readout;
-	document.getElementById("count").value = "PAUSE"; } 
-	clocktimer = setTimeout("startTIME()",1); 
+	if (show == true) { document.TestForm.stopwatch.value = readout;
+	document.getElementById("count").value = "PAUSE"; }
+	clocktimer = setTimeout("startTIME()",10);
 	} 
 
 
@@ -75,7 +76,7 @@ function clearСlock() {
 	msec = 0;
 	init = 0;
 	show = true;
-	readout='00:00:00.00';
+	readout='00:00:00.000';
 	document.TestForm.stopwatch.value = readout;
 	document.getElementById("count").value = 'START';
 
